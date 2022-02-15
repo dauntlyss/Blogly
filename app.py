9e6afaf3efc585212fa9b95c6da46fd29c11d7c2
Alyssa Harris <amh302918@gmail.com>
2022-02-15 12:30:10 -0500

    removed echo config

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from models import db, connect_db, User, Post
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///blogly"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SQLALCHEMY_ECHO'] = True
 app.config["SECRET_KEY"] = "girlslovebeyonce"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
